98dcf62495b403613c6cd978df5cac51dacdb51a
aleksi2105 <aleksi2105@yandex.ru>
2026-06-24 03:26:57 +0300

    fix: setInterval func

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
 import timer from "./modules/timer.js";
 
-timer('25 june 2026')
+timer('25 may 2026')
--- src/modules/timer.js
+++ src/modules/timer.js
@@ -3,11 +3,21 @@ const timer = function (deadline) {
   const timerMinutes = document.getElementById('timer-minutes')
   const timerSeconds = document.getElementById('timer-seconds')
 
+  let intervalId
+
+  const numZero = (num) => {
+    return String(num).padStart(2, '0')
+  }
+
   const getTimeRemaining = () => {
 
     let dateStop = new Date(deadline).getTime()
     let dateNow = new Date().getTime()
     let timeRemaining = (dateStop - dateNow) / 1000
+    if (timeRemaining <= 0) {
+      return { timeRemaining: 0, hours: 0, minutes: 0, seconds: 0 }
+    }
+
     let hours = Math.floor(timeRemaining / 60 / 60)
     let minutes = Math.floor((timeRemaining / 60) % 60)
     let seconds = Math.floor(timeRemaining % 60)
@@ -18,16 +28,20 @@ const timer = function (deadline) {
   const updateClock = () => {
     let getTime = getTimeRemaining()
 
-    timerHours.textContent = getTime.hours
-    timerMinutes.textContent = getTime.minutes
-    timerSeconds.textContent = getTime.seconds
+    timerHours.textContent = numZero(getTime.hours)
+    timerMinutes.textContent = numZero(getTime.minutes)
+    timerSeconds.textContent = numZero(getTime.seconds)
 
-    if (getTime.timeRemaining > 0) {
-      setTimeout(updateClock, 1000)
+    if (getTime.timeRemaining === 0) {
+      clearInterval(intervalId)
+      // timerHours.textContent = '00'
+      // timerMinutes.textContent = '00'
+      // timerSeconds.textContent = '00'
     }
-
+    console.log('Проверка');
   }
   updateClock()
+  intervalId = setInterval(updateClock, 1000)
 }
 
 export default timer
