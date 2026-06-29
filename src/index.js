019d1214bbf149ed427d459c4ba79f664a173e2f
aleksi2105 <aleksi2105@yandex.ru>
2026-06-29 03:52:15 +0300

    add: calculate, number validation

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import timer from "./modules/timer.js";
 import menu from './modules/menu.js'
 import modal from "./modules/modal.js";
+import calc from "./modules/calculate.js";
 
 timer('25 may 2026')
 menu()
 modal()
+calc()
