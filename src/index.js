934083b18e0067cdc6cd2f8073d4bf7a196f1859
aleksi2105 <aleksi2105@yandex.ru>
2026-07-02 03:51:16 +0300

    add: script for tabs

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import menu from './modules/menu.js'
 import modal from "./modules/modal.js";
 import calc from "./modules/calculate.js";
 import forms from "./modules/forms.js";
+import tabs from "./modules/tabs.js";
 
 timer('25 may 2026')
 menu()
 modal()
 calc()
 forms()
+tabs()
